e112b5e25ea3ff8ca05f3ed4d89914fe68f45a36
Vijaylaxmi Nagurkar <vijaylaxmidighe@gmail.com>
2020-12-07 01:34:25 -0800

    Added UI tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
+test('App renders properly without any error', () => {
   let wrapped = shallow(<App></App>);
   expect(wrapped).toMatchSnapshot();
 });
